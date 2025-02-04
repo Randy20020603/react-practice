a3f9ee2da4e0a0f803fc438d7a0c1f7b1eccb6fa
Randy Luo <117186456+Randy20020603@users.noreply.github.com>
2025-02-04 16:52:42 +0100

    fix

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,5 +1,11 @@
 function ListGroup() {
-  const items = [];
+  const items = [
+    "Amsterdam",
+    "Hoofddorp",
+    "Haarlem",
+    "Rotterdam",
+    "Vijfhuizen",
+  ];
 
   const getMessage = () => {
     return items.length === 0 ? <p>No items found</p> : null;
@@ -8,14 +14,21 @@ function ListGroup() {
   return (
     <>
       <h1>Hello Tyrell</h1>
-      {/*One way of dong it*/}
+      {/*One way of dong it
+        Get's error in console*/}
       {getMessage()}
       {/*Anotherway of doing it*/}
       {items.length === 0 && <p>geen item gevonden</p>}
 
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("Hi")}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
