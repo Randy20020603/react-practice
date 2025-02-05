a25ff2985ad00faa784bf5293946537eb73ce400
Randy Luo <117186456+Randy20020603@users.noreply.github.com>
2025-02-05 10:06:03 +0100

    hook, useState

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ListGroup() {
   const items = [
     "Amsterdam",
@@ -7,25 +9,45 @@ function ListGroup() {
     "Vijfhuizen",
   ];
 
+  /* Hook
+  useState is een hook die je gebruikt om state toe te voegen aan je functionele componenten.
+  Het retourneert een array met twee waarden: de state zelf en een functie om de state bij te werken.
+  Wanneer je de state bijwerkt, wordt de component opnieuw gerenderd met de nieuwe state-waarde,
+   waardoor je dynamische en interactieve toepassingen kunt bouwen.
+  */
+  const [selectedItem, setSelectedItem] = useState(-1);
+
   const getMessage = () => {
     return items.length === 0 ? <p>No items found</p> : null;
   };
 
+  // Event handler
+  const handleClick = (event) => console.log("hi");
+
   return (
     <>
       <h1>Hello Tyrell</h1>
-      {/*One way of dong it
-        Get's error in console*/}
+      {/* One way of dong it
+        Get's error in console */}
       {getMessage()}
-      {/*Anotherway of doing it*/}
+      {/* Anotherway of doing it:
+        if the left side is true, then render the right side
+      */}
       {items.length === 0 && <p>geen item gevonden</p>}
 
       <ul className="list-group">
-        {items.map((item) => (
+        {/* The second parameter will automaticly become the index*/}
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedItem === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log("Hi")}
+            onClick={() => {
+              setSelectedItem(index);
+            }}
           >
             {item}
           </li>
