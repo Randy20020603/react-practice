c47ce0625009e7849bb88dab4ba456d217974391
Randy Luo <117186456+Randy20020603@users.noreply.github.com>
2025-02-06 10:35:43 +0100

    style added at Greeting

--- src/components/UserGreeting.jsx
+++ src/components/UserGreeting.jsx
@@ -1,4 +1,4 @@
 function UserGreeting(props) {
-    return(props.isLoggedIn ? <h1>Welcome!</h1> : <h1>Please log in</h1>)
+    return(props.isLoggedIn ? <h1>Welcome!</h1> : <h1 style={{color: "red", backgroundColor: "gray"}}>Please log in</h1>)
 }
 export default UserGreeting
